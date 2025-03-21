945d96a0ef467bd2193bcd5399730a9d34def4c1
nomanshaikh07 <nomanshaikh9518@gmail.com>
2025-03-21 21:50:23 +0530

    commited

--- data/INGESTION/customerReviews_API.py
+++ data/INGESTION/customerReviews_API.py
@@ -9,7 +9,7 @@ from google.cloud import storage
 spark = SparkSession.builder.appName("CustomerReviewsAPI").getOrCreate()
 
 # ✅ API Endpoint
-API_URL = "https://67da957935c87309f52d2fc0.mockapi.io/reviews"
+API_URL = "https://67dcb3d8e00db03c4068ad49.mockapi.io/reviews"
 
 # ✅ Step 1: Fetch data from API
 response = requests.get(API_URL)
@@ -28,7 +28,7 @@ today = datetime.datetime.today().strftime('%Y%m%d')  # Format: YYYYMMDD
 
 # ✅ Step 4: Define File Paths with Date
 local_parquet_file = f"/tmp/customer_reviews_{today}.parquet"
-GCS_BUCKET = "datalake-project-bkt-19032025"
+GCS_BUCKET = "avd-datalake"
 GCS_PATH = f"landing/customer_reviews/customer_reviews_{today}.parquet"
 
 # ✅ Step 5: Save Pandas DataFrame as Parquet Locally
