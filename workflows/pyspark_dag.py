5049e6b6a51339a555320f432235cfbd78529bde
nomanshaikh07 <nomanshaikh9518@gmail.com>
2025-03-21 22:28:04 +0530

    commit all by noman

--- workflows/pyspark_dag.py
+++ workflows/pyspark_dag.py
@@ -10,7 +10,7 @@ from airflow.providers.google.cloud.operators.dataproc import (
 )
 
 # define the variables
-PROJECT_ID = "avd-databricks-demo"
+PROJECT_ID = "nomanproject"
 REGION = "us-east1"
 CLUSTER_NAME = "my-demo-cluster"
 COMPOSER_BUCKET = "us-central1-demo-instance-968eb987-bucket"
