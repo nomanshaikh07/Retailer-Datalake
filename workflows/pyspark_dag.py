93ea74ddd10f9ee4f40be290a35bcdb69a2eb9f6
nomanshaikh07 <nomanshaikh9518@gmail.com>
2025-04-06 16:23:35 +0530

    3

--- workflows/pyspark_dag.py
+++ workflows/pyspark_dag.py
@@ -13,7 +13,7 @@ from airflow.providers.google.cloud.operators.dataproc import (
 PROJECT_ID = "nomanproject"
 REGION = "us-east1"
 CLUSTER_NAME = "my-demo-cluster"
-COMPOSER_BUCKET = "us-central1-new-7747f23f-bucket"
+COMPOSER_BUCKET = "us-central1-demo-b1a333dc-bucket"
 
 GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/INGESTION/retailerMysqlToLanding.py"
 PYSPARK_JOB_1 = {
